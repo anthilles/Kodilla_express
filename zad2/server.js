a57d65f5c2accc71060f5cf679a42b14bc2e01b1
anthilles <anthilles@outlook.com>
2017-10-04 14:08:34 +0200

    Zadanie2

---
+++ zad2/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('assets'));
+app.get('/userform', function (req, res) {
+    const response = {
+        FullName: {
+            first_name: req.query.first_name,
+            last_name: req.query.last_name
+        }
+    };
+    res.end(JSON.stringify(response));
+});
+
+const server = app.listen(3000, 'localhost', function() {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('serwer http://' + host + ':' + port);
+});
