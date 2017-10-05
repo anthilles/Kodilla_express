e4fc8a64677d686ebdb8b163b494df226f831337
anthilles <anthilles@outlook.com>
2017-10-05 12:34:30 +0200

    Zad 5

---
+++ zad5/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID':'676942891686-34ck1f4b44kjud25k572tat616micmhu.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'DN4sc8apNSjkmsbNlKWCyiT9',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
